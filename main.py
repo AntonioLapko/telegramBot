1a870dfebe91f0a8bb7bbdd111992f1c314a624a
Anatoly <constructor-08@yandex.ru>
2025-11-04 02:53:37 +0400

    add Dockerfile

--- main.py
+++ main.py
@@ -4,8 +4,8 @@
 #docker build -t lapin_telegram_bot .
 #docker images
 #docker run lapin_telegram_bot
-#docker build . -t cr.yandex/crplo5125gv8esvpt42k/lapin_telegram_bot:1.0.7
-#docker push cr.yandex/crplo5125gv8esvpt42k/lapin_telegram_bot:1.0.7
+#docker build . -t cr.yandex/crplo5125gv8esvpt42k/lapin_telegram_bot:1.0.9
+#docker push cr.yandex/crplo5125gv8esvpt42k/lapin_telegram_bot:1.0.9
 #docker tag 9f3a31473d6a cr.yandex/crplo5125gv8esvpt42k/lapin_telegram_bot:1.0.2
 
 #TOKEN = "8482269363:AAEetzUmFKJGhgx9lCFBQHQptb-LMMJxbZ0"
@@ -24,22 +24,24 @@
 
 #Проверьте URL репозитория git remote -v
 
-
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from telegram import Update
 from telegram.ext import Application, MessageHandler, filters, ContextTypes
 import json
-import asyncio
 import logging
+import asyncio
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 TOKEN = "8482269363:AAEetzUmFKJGhgx9lCFBQHQptb-LMMJxbZ0"
 application = None
+http_server = None
+
 
 async def echo_icho(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("И чё?")
+
 
 class WebhookHandler(BaseHTTPRequestHandler):
     def do_POST(self):
@@ -52,8 +54,12 @@ class WebhookHandler(BaseHTTPRequestHandler):
             body = self.rfile.read(content_length)
             data = json.loads(body.decode('utf-8'))
 
-            application.create_task(
-                application.update_queue.put(Update.de_json(data))
+            # Получаем loop из глобального контекста
+            loop = asyncio.get_event_loop()
+            # Планируем задачу в loop
+            asyncio.run_coroutine_threadsafe(
+                application.update_queue.put(Update.de_json(data)),
+                loop
             )
 
             self.send_response(200)
@@ -68,13 +74,28 @@ class WebhookHandler(BaseHTTPRequestHandler):
             logger.error(f"Ошибка обработки запроса: {e}")
             self.send_error(500, "Internal Server Error")
 
+
 def main():
-    global application
+    global application, http_server
     application = Application.builder().token(TOKEN).build()
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo_icho))
-    server = HTTPServer(('0.0.0.0', 8080), WebhookHandler)
-    logger.info("Бот слушает HTTP на 0.0.0.0:8080")
-    server.serve_forever()
+
+    # Запускаем приложение Telegram в фоне
+
+    application.run_webhook(
+        listen='0.0.0.0',
+        port=8080,
+        url_path='/bot',
+        webhook_url='https://d5dgn53kgfesereq4a0j.zj2i1qoy.apigw.yandexcloud.net/bot'  # укажите реальный URL
+    )
+
+    try:
+        http_server.serve_forever()
+    except KeyboardInterrupt:
+        logger.info("Сервер остановлен")
+    finally:
+        http_server.server_close()
+
 
 if __name__ == '__main__':
     main()
